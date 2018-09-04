fafe7c004a081b7fefeb5bd02453b88fa00b4f2c
sudha917 <sudha.avinash917.com>
2018-09-04 15:38:26 +0530

    Interface Concept

---
+++ Interfaceconcept/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaceconcept
+{
+    interface Father
+    {
+         void FAddress();
+    }
+    interface Mother: Father
+    {
+        void MAddress();
+    }
+
+    class Son : Father, Mother
+    {
+        public void FAddress()
+        {
+            Console.WriteLine("Father Address");
+        }
+
+        public void MAddress()
+        {
+            Console.WriteLine("Mother Address");
+        }
+    }
+    class Program: Son
+    {
+        static void Main(string[] args)
+        {
+            Son s = new Son();
+            s.FAddress();
+            s.MAddress();
+            Console.ReadLine();
+        }
+    }
+}
